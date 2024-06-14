1dacae1e8d163028b3c3e4fcaa48ec1a720b1c81
brett <brett.hamm@gmail.com>
2024-06-14 15:42:00 -0600

    fixing image paths for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://goodenoughgaming.github.io',
-  base: './',
+  site: 'https://goodenoughgaming.com',
+  base: '/',
   integrations: [tailwind()]
 });
